fcc0ff1862fad0ec8e948af3b6f1ed7d2e64e6d1
Mark Kantor <kantor.mark@gmail.com>
2016-12-08 17:24:37 +0100

    Removed using _context

--- src/RatingApp.Web/Controllers/Api/SkillsController.cs
+++ src/RatingApp.Web/Controllers/Api/SkillsController.cs
@@ -8,6 +8,7 @@ using System.Security.Claims;
 using System;
 using Microsoft.EntityFrameworkCore;
 using RatingApp.Web.Controllers.Models;
+using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -26,53 +27,54 @@ namespace RatingApp.Web.Controllers.Api
         } 
       
         [HttpGet]
-        public IEnumerable<Skill> GetAll()
+        public async Task<IEnumerable<Skill>> GetAll()
         {
-            return _context.Skills.ToList();
+            return await _context.Skills.ToListAsync();
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public Skill Get(int id)
+        public async Task<Skill> Get(int id)
         {
-            return _context.Skills.FirstOrDefault(e => e.SkillId == id);
+            return await _context.Skills.FirstOrDefaultAsync(e => e.SkillId == id);
         }
 
         //[Route("api/search/{value}")]
 
         [HttpPost]
-        public JsonResult Search([FromBody]SearchModel model)
+        public async Task <JsonResult> Search([FromBody]SearchModel model)
         {
-            var result = _context.Skills.Where(data => data.SkillName.StartsWith(model.SearchTerm.Trim())).ToList();
+            var result = await _context.Skills.Where(data => data.SkillName.StartsWith(model.SearchTerm.Trim())).ToListAsync();
 
             if (result.Count==0 && !string.IsNullOrWhiteSpace(model.SearchTerm)){
 
-                var skill = new Skill
-                {
-                    SkillId = 0,
-                    SkillName = model.SearchTerm
-                };
-                result = new List<Skill>();
-                result.Add(skill);
+           
 
+                    var skill = new Skill
+                    {
+                        SkillId = 0,
+                        SkillName = model.SearchTerm
+                    };
+                    result = new List<Skill>();
+                    result.Add(skill);
             }
             return Json(result);
         }
 
         [HttpGet]
-        public JsonResult UserSkills() { 
+        public async Task<JsonResult> UserSkills() { 
 
              var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
             {
-                var userSkills = _context.UserSkills.Include(p=> p.Skill).OrderBy(p=>p.Skill.SkillName).Where(u => u.UserId == userId).ToList();
+                var userSkills = await _context.UserSkills.Include(p=> p.Skill).OrderBy(p=>p.Skill.SkillName).Where(u => u.UserId == userId).ToListAsync();
                 return Json(userSkills);
             }
            
         }
 
         [HttpPost]
-        public IActionResult Add([FromBody]SkillAddModel model)
+        public async Task<IActionResult> Add([FromBody]SkillAddModel model)
         {
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
@@ -82,7 +84,7 @@ namespace RatingApp.Web.Controllers.Api
                 return BadRequest();
 
             //Do not allow another set of skill and user in DB
-            if (_context.UserSkills.Any(p => p.SkillId == skillId && p.UserId == userId))
+            if (await _context.UserSkills.AnyAsync(p => p.SkillId == skillId && p.UserId == userId))
             {
 
                 return BadRequest();
@@ -91,14 +93,14 @@ namespace RatingApp.Web.Controllers.Api
             //New skill to be added
             if (skillId == 0)
             {
-                var skill = new Skill
-                {
-                    SkillName = model.SkillName
-                };
-                _context.Skills.Add(skill);
-                _context.SaveChanges();
-                skillId = skill.SkillId;
-            }
+             var skill = new Skill
+                    {
+                        SkillName = model.SkillName
+                    };
+                    _context.Skills.Add(skill);
+                    await _context.SaveChangesAsync();
+                    skillId = skill.SkillId;
+                }
 
             else
             {
@@ -117,26 +119,30 @@ namespace RatingApp.Web.Controllers.Api
                 SkillId = skillId,
                 Added = DateTime.Now
             };
+
                 _context.UserSkills.Add(userSkill);
-                _context.SaveChanges();
+                await _context.SaveChangesAsync();
 
                 return Ok();
+            
         }
 
         [HttpDelete]
-        public IActionResult UserSkills([FromBody] UserSkillDeleteModel model)
+        public async Task<IActionResult> UserSkills([FromBody] UserSkillDeleteModel model)
         {
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             int skillIId = int.Parse(model.SkillId);
 
-            var userSkill = _context.UserSkills.FirstOrDefault(p => p.SkillId == skillIId && p.UserId == userId);
+            var userSkill = await _context.UserSkills.FirstOrDefaultAsync(p => p.SkillId == skillIId && p.UserId == userId);
 
             if (userSkill != null)
             {
 
-                _context.UserSkills.Remove(userSkill);
-                _context.SaveChanges();
-                return Ok();
+           
+                    _context.UserSkills.Remove(userSkill);
+                    await _context.SaveChangesAsync();
+                    return Ok();
+                
 
             }
             else
@@ -145,7 +151,7 @@ namespace RatingApp.Web.Controllers.Api
 
         
 
-        public IActionResult UserSkillLevel ([FromBody]UserSkillLevelModel model)
+        public async Task<IActionResult> UserSkillLevel ([FromBody]UserSkillLevelModel model)
         {
 
             if (ModelState.IsValid)
@@ -159,7 +165,7 @@ namespace RatingApp.Web.Controllers.Api
                 if (level < 1)
                     return BadRequest();
 
-                var userSkill = _context.UserSkills.Where(p => p.SkillId == skillIId && p.UserId == userId).FirstOrDefault();
+                var userSkill = await _context.UserSkills.Where(p => p.SkillId == skillIId && p.UserId == userId).FirstOrDefaultAsync();
 
                 if (userSkill == null) {
                     return NotFound();
@@ -167,7 +173,7 @@ namespace RatingApp.Web.Controllers.Api
                 else {
                     userSkill.Level = level;
                     userSkill.Updated = DateTime.Now;
-                    _context.SaveChanges();
+                    await _context.SaveChangesAsync();
                 }
                 return Ok();
             }
