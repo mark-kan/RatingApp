52deb97f9fe896a5195733760e343c77efbee1fb
Mark Kantor <kantor.mark@gmail.com>
2016-12-13 20:56:20 +0100

    Created Composite Key of UserSkill

--- src/RatingApp.Domain/UserSkill.cs
+++ src/RatingApp.Domain/UserSkill.cs
@@ -8,15 +8,20 @@ namespace RatingApp.Domain
     public class UserSkill
     {
       
-        [ForeignKey("Skill")]
+        [Key]
+        [Column(Order=0)]
         public int SkillId { get; set; }
 
-        public virtual Skill Skill { get; set; }
-        
+        [Key]
+        [Column(Order = 1)]
         public string UserId { get; set; }
 
         [Range(0, 5)]
         public int? Level { get; set; }
+
+        [ForeignKey("SkillId")]
+        public virtual Skill Skill { get; set; }
+
 
         [Required]
         public DateTime Added { get; set; }
